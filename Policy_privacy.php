038bc9f85b974fda026ac75855f8d58e12e59f50
Louce <79318424+Louce@users.noreply.github.com>
2021-04-05 21:55:53 +0200

    modification footer et pages associées
    page contact et page mentions légales

--- Policy_privacy.php
+++ Policy_privacy.php
@@ -6,33 +6,33 @@
         <link rel="stylesheet" href="Style.css" type="text/css" />
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
         <title>GBAF: Groupement Banque-Assurance Français</title>
     </head>
 
+<!--page de base quand l'utilisateur n'est pas connecté ou s'est déconnecté-->
     <body>
 
     <div class="header">
          <a href="connexion.php"><img class="logo" src="Visuels/logo_fond blanc2.png" alt="Logo GBAF: Groupement Banque Assurance Français" title="Logo GBAF" /></a>
     </div>   
-
+       
     	<div id="contact">
 
-    		<div class="info">
-    		
-    			<h2>COORDONNEES</h2>
+            <div class="info">
 
-    			<p>GBAF: Groupement Banque Assurance Français</p>
-    			<p>1 avenue de l'argent <br/> 92800 PUTEAUX</p>
-    			<p>Tel: 01 58 61 97 52</p>
-
-    		</div>
-    	</div>
+            <h1>MENTIONS LEGALES</h1>
+            
+            	<p>
+            		Blablabla
+            	</p>
+            </div>   
+            
+        </div>
 
     <footer>
         <?php include("_footer.php"); ?> 
     </footer>
-    
-    </body>
 
+    </body>
 </html>
--- _footer.php
+++ _footer.php
@@ -1,5 +1,7 @@
 <!-- Mentions légales + contact-->
 <footer>
-		<a class="afooter" href="06_mentions_legales.php">Mentions Légales</a>
-		<a class="afooter" href="05_contact.php">Contact</a>
+	<div>
+		<a class="afooter" href="Policy_privacy.php">Mentions Légales</a>
+		<a class="afooter" href="Contact.php">Contact</a>
+	</div>
 </footer>
